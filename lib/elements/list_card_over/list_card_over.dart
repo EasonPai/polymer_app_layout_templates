03bb6c89b91d22b48dcf3f19646cc699001d05dc
Eason <mediakid@gmail.com>
2016-06-01 10:13:56 +0800

    - supporting neon animation pages

--- lib/elements/list_card_over/list_card_over.dart
+++ lib/elements/list_card_over/list_card_over.dart
@@ -10,6 +10,14 @@ import 'package:polymer_app_layout_template/behaviors/behaviors.dart';
 import 'package:polymer_app_layout_template/elements/elements.dart';
 import 'package:web_components/web_components.dart' show HtmlImport;
 
+import 'package:polymer_elements/neon_animation/animations/slide_from_left_animation.dart';
+import 'package:polymer_elements/neon_animation/animations/slide_from_right_animation.dart';
+import 'package:polymer_elements/neon_animation/animations/slide_right_animation.dart';
+import 'package:polymer_elements/neon_animation/animations/slide_left_animation.dart';
+import 'package:polymer_elements/neon_animation/animations/slide_left_animation.dart';
+import 'package:polymer_elements/neon_animated_pages.dart';
+import 'package:polymer_elements/neon_animatable_behavior.dart';
+
 @PolymerRegister('layout-list-card-over')
 class LayoutListCardOver extends PolymerElement
     with PolymerRouteBehavior, ToolbarBehavior, LeftNavBehavior, IconBehavior {
